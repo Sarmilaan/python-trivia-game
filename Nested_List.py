b3e389bba3ae9196a6602ca5ecb905d0c1c46029
Sarmilaa Naidu <sarmilaanaidu@gmail.com>
2025-12-24 19:38:41 +0530

    Keep only trivia game file

--- Nested_List.py
+++
@@ -1,7 +0,0 @@
-m=[]
-for i in range(5):
-        m.append([])               # Add empty row
-        for j in range(5):
-                m[i].append(j)         # Add values 0–4 in each row
-
-print(m)
